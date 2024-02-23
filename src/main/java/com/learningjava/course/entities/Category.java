e17de1c3700be1a1ade360e0310f764301d1eff7
Guilherme Suganame <gsuganame@gmail.com>
2024-02-23 15:08:13 -0300

    feat: create many-to-many association with jointable

--- src/main/java/com/learningjava/course/entities/Category.java
+++ src/main/java/com/learningjava/course/entities/Category.java
@@ -4,10 +4,13 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
@@ -19,8 +22,9 @@ public class Category implements Serializable {
     private Long id;
     private String name;
 
-    @Transient
-    private Set<Product> products = new HashSet<>();    
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
 
     private Category() {
     }
