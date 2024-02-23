4920d9847783b846fc812804544feba2254a0d10
Guilherme Suganame <gsuganame@gmail.com>
2024-02-23 14:35:10 -0300

    feat: category entity and implementation

--- src/main/java/com/learningjava/course/config/TestConfig.java
+++ src/main/java/com/learningjava/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.learningjava.course.entities.Category;
 import com.learningjava.course.entities.Order;
 import com.learningjava.course.entities.User;
 import com.learningjava.course.entities.enums.OrderStatus;
+import com.learningjava.course.repositories.CategoryRepository;
 import com.learningjava.course.repositories.OrderRepository;
 import com.learningjava.course.repositories.UserRepository;
 
@@ -20,8 +22,12 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private UserRepository userRepository;
+
     @Autowired
     private OrderRepository orderRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -29,11 +35,16 @@ public class TestConfig implements CommandLineRunner {
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "888888888", "123456");
 
         Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
-        Order o3 = new Order(null, Instant.parse("2019-06-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-06-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
+
+        Category cat1 = new Category(null, "Eletronics");
+        Category cat2 = new Category(null, "Books");
+        Category cat3 = new Category(null, "Computers");
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+        categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
     }
 
