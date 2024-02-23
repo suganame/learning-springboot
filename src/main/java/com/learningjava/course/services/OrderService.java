afa98896e734a5ea1c6c1f6868800297cd7a90fc
Guilherme Suganame <gsuganame@gmail.com>
2024-02-23 12:21:51 -0300

    feat: setup Instant, ISO 8601 and Order resources

---
+++ src/main/java/com/learningjava/course/services/OrderService.java
@@ -0,0 +1,25 @@
+package com.learningjava.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.learningjava.course.entities.Order;
+import com.learningjava.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+    @Autowired
+    private OrderRepository repository;
+
+    public List<Order> findAll() {
+        return repository.findAll();
+    }
+
+    public Order findById(Long id) {
+        Optional<Order> obj = repository.findById(id);
+        return obj.get();
+    }
+}
