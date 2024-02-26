5fc8dca47edcaee1b61ba065f282abad8f610cfd
Guilherme Suganame <gsuganame@gmail.com>
2024-02-26 14:25:29 -0300

    feat: create subtotal and total methods

--- src/main/java/com/learningjava/course/entities/Order.java
+++ src/main/java/com/learningjava/course/entities/Order.java
@@ -95,6 +95,14 @@ public class Order implements Serializable {
         this.payment = payment;
     }
 
+    public Double getTotal() {
+        Double sum = 0.0;
+        for (OrderItem oi : items) {
+            sum += oi.getSubTotal();
+        }
+        return sum;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
--- src/main/java/com/learningjava/course/entities/Payment.java
+++ src/main/java/com/learningjava/course/entities/Payment.java
@@ -3,6 +3,8 @@ package com.learningjava.course.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,6 +23,7 @@ public class Payment implements Serializable {
 
     @OneToOne
     @MapsId
+    @JsonIgnore
     private Order order;
 
     public Payment() {
