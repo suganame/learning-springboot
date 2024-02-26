9b9a999ab61db05a0998dd5793638491d7834ed8
Guilherme Suganame <gsuganame@gmail.com>
2024-02-26 15:40:57 -0300

    feat: create exception handler - delete method

--- src/main/java/com/learningjava/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/learningjava/course/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.learningjava.course.services.exceptions.DatabaseException;
 import com.learningjava.course.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,6 +19,15 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         String err = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError error = new StandardError(Instant.now(), status.value(), err, e.getMessage(),
+                request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        String err = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError error = new StandardError(Instant.now(), status.value(), err, e.getMessage(),
                 request.getRequestURI());
         return ResponseEntity.status(status).body(error);
--- src/main/java/com/learningjava/course/services/UserService.java
+++ src/main/java/com/learningjava/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.learningjava.course.entities.User;
 import com.learningjava.course.repositories.UserRepository;
+import com.learningjava.course.services.exceptions.DatabaseException;
 import com.learningjava.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -29,11 +32,21 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            if (!repository.existsById(id)) {
+                throw new ResourceNotFoundException(id);
+            }
+            repository.deleteById(id);
+        } catch (ResourceNotFoundException e) {
+            throw new ResourceNotFoundException(e.getMessage());
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
-        // getReferenceById não busca as informações no db. Ele prepara o obj e depois envia para o banco de dados
+        // getReferenceById não busca as informações no db. Ele prepara o obj e depois
+        // envia para o banco de dados
         User entity = repository.getReferenceById(id);
         updateData(entity, obj);
         return repository.save(entity);
